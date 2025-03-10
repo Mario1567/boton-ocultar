4206ddd8f72c036e1296fd2d524876554be217b8
Mario1567 <maamon1213@gmail.com>
2025-03-10 08:45:34 +0100

    cambios
    cambios

---
+++ script.js
@@ -0,0 +1,7 @@
+// Esperar a que el documento esté listo
+$(document).ready(function() {
+    // Función para ocultar el párrafo al hacer clic en el botón
+    $('#ocultar-btn').click(function() {
+        $('#parrafo').hide(); // Oculta el párrafo
+    });
+});
